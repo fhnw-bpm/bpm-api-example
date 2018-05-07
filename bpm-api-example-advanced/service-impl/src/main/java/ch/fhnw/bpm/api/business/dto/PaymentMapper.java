3d04fa24cf0a9787b3625043e9a593d83663d83f
Andreas Martin <andreas.martin@andreasmartin.ch>
2018-05-07 13:21:54 +0200

    Update

--- bpm-api-example-advanced/service-impl/src/main/java/ch/fhnw/bpm/api/business/dto/PaymentMapper.java
+++ bpm-api-example-advanced/service-impl/src/main/java/ch/fhnw/bpm/api/business/dto/PaymentMapper.java
@@ -7,11 +7,15 @@ package ch.fhnw.bpm.api.business.dto;
 
 import ch.fhnw.bpm.api.data.domain.PaymentEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
 
+    @Mappings({@Mapping(target = "order.payment", ignore = true), @Mapping(target = "order.customer", ignore = true)})
     PaymentDTO entityToDto(PaymentEntity entity);
+    @Mappings({@Mapping(target = "order.payment", ignore = true), @Mapping(target = "order.customer", ignore = true)})
     PaymentEntity dtoToEntity(PaymentDTO dto);
 
 }
